b9cc957a89c906e08b013dd6d9604dca826b8afb
tofhddl9 <tofhddl9@gmail.com>
2020-10-04 03:45:34 +0900

    Modify resititution of ball to 0.2

--- PinballGame/PinballGame/ball.cpp
+++ PinballGame/PinballGame/ball.cpp
@@ -16,7 +16,7 @@ Ball::Ball(b2World* world, const b2Vec2 pos, const float radius) {
     b2FixtureDef fixture_def;
     fixture_def.density = 1.0f;
     fixture_def.friction = 0.36f;
-    fixture_def.restitution = 0.6f;
+    fixture_def.restitution = 0.2f;
     fixture_def.shape = &shape;
 
     body_->CreateFixture(&fixture_def);
@@ -48,12 +48,12 @@ void Ball::Render()
     glPushMatrix();
     glTranslatef(body_->GetPosition().x, body_->GetPosition().y, 0.0f);
     glRotatef(body_->GetAngle(), 0.0f, 0.0f, 1.0f);
-    glColor3f(0.9f, 0.2f, 0.4f);
+    glColor3f(0.7f, 0.9f, 0.9f);
 
     float theta;
     glBegin(GL_POLYGON);
     for (int i = 0; i < 360; i++) {
-        theta = 3.142 * i / 180;
+        theta = b2_pi * i / 180;
         glVertex2f(radius_ * cos(theta), radius_ * sin(theta));
     }
     glEnd();
