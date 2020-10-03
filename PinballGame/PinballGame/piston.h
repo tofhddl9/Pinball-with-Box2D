e427dbbb797e0d3fcf86bab8935182bafd9b034a
tofhddl9 <tofhddl9@gmail.com>
2020-10-03 19:51:55 +0900

    Add Piston class

---
+++ PinballGame/PinballGame/piston.h
@@ -0,0 +1,28 @@
+#ifndef __PISTON_H__
+#define __PISTON_H__
+
+#pragma once
+#include "Box2D/Box2D.h"
+#include "GL/glut.h"
+
+class Piston
+{
+public:
+    Piston(b2World *world, const b2Vec2 pivot_pos,
+        const b2Vec2 head_pos, const float radius);
+    void Pull();
+    void Push(int time);
+    void Render();
+
+private:
+    void CreateJoint(b2World* world, b2Body* pivot, b2Body* head);
+
+    const float FORCE_PER_PRESSED_TIME = 200.0;
+    float head_radius_;
+    b2Body* head_;
+    b2Body* pivot_;
+    b2PrismaticJoint* joint_;
+};
+
+
+#endif
